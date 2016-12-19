2c0b2c5e5cdfe0331672006b05a9b651a0850126
ratty3697 <koolboyprabhat@gmail.com>
2016-12-19 21:14:18 +0530

    cors headers added

--- NatureInspiredHybridAlgoToolbox/settings.py
+++ NatureInspiredHybridAlgoToolbox/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'batAlgorithm',
     'ABC',
     'pso',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'NatureInspiredHybridAlgoToolbox.urls'
 
